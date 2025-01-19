a42c91f67551e78b386f7075e1c476d611acfdd9
Anshul Kumar <anshulisokay@gmail.com>
2025-01-19 10:52:49 +0530

    Updated

--- app/src/main/java/com/example/magicquote/MainActivity.kt
+++ app/src/main/java/com/example/magicquote/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.magicquote
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.magicquote.components.HeaderBlock
 import com.example.magicquote.components.MagicLayout
+import com.example.magicquote.navigation.MagicNav
+import com.example.magicquote.navigation.ScreenNav
+import com.example.magicquote.quoteViewModel.MagicViewModel
 import com.example.magicquote.screenLayout.OfflineQuote
 import com.example.magicquote.ui.theme.MagicQuoteTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,18 +38,38 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
+        //Restriction to Screen Capture
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE)
+        // Set the status bar color to black
+        window.statusBarColor = android.graphics.Color.BLACK
+
         setContent {
+            val viewModel: MagicViewModel = viewModel()
             MagicQuoteTheme {
-                CombinedFrame()
+                CombinedFrame(viewModel)
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CombinedFrame(){
-    MagicLayout {
-        OfflineQuote()
-    }
+fun CombinedFrame(viewModel: MagicViewModel){
+    val navController: NavHostController = rememberNavController()
+    MagicLayout(topBar = { HeaderBlock({ Text("Magic Quote") }
+    )},
+        floatingActionButton = {
+            FloatingActionButton(onClick = {viewModel.floatingButtonAction(navHostController =navController )}) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        },
+        content = {
+        MagicNav(viewModel = viewModel,
+            paddingValues = it,
+            navController = navController,
+            startDestination = ScreenNav.HomeScreen.name)
+    })
 }
--- app/src/main/java/com/example/magicquote/quoteViewModel/MagicViewModel.kt
+++ app/src/main/java/com/example/magicquote/quoteViewModel/MagicViewModel.kt
@@ -1,9 +1,49 @@
 package com.example.magicquote.quoteViewModel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavHostController
+import com.example.magicquote.navigation.ScreenNav
+import kotlinx.coroutines.launch
 
 
-@HiltViewModel
 class MagicViewModel:ViewModel() {
+
+    private val _isInputDrawerExpanded = mutableStateOf(true)
+    val isInputDrawerExpanded: State<Boolean> get() = _isInputDrawerExpanded
+
+    // Toggles the expand state
+     fun toggleExpandState() {
+        viewModelScope.launch {
+            _isInputDrawerExpanded.value = !_isInputDrawerExpanded.value
+        }
+    }
+
+    // Function to handle Floating Button Action
+    fun floatingButtonAction(navHostController: NavHostController) {
+        val navState = navHostController.currentBackStackEntry?.destination?.route == ScreenNav.HomeScreen.name
+
+        when {
+            // Case 1: Button is collapsed and already on HomeScreen -> Expand it
+            !_isInputDrawerExpanded.value && navState -> {
+                toggleExpandState()
+            }
+
+            // Case 2: Not on HomeScreen -> Navigate to HomeScreen and expand
+            !navState -> {
+                navHostController.navigate(ScreenNav.HomeScreen.name) {
+                    launchSingleTop = true
+                    restoreState = true
+                }
+                toggleExpandState()
+            }
+
+            // Case 3: Button is expanded and already on HomeScreen -> Save the quote
+            _isInputDrawerExpanded.value && navState -> {
+               // saveQuote()
+            }
+        }
+    }
 }
